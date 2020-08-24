ee91a6430c8fc7ad33adcaad8cf2d935d3c3ee4c
wupeiwen <javapeiwen2010@gmail.com>
2020-08-24 18:56:23 +0800

    deps: 移除无效依赖

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -2,14 +2,10 @@
  * @Author: wupeiwen <javapeiwen2010@gmail.com>
  * @Date: 2020-08-10 16:53:52
  * @LastEditors: wupeiwen <javapeiwen2010@gmail.com>
- * @LastEditTime: 2020-08-19 09:36:28
+ * @LastEditTime: 2020-08-24 17:21:54
  */
 import Vue from 'vue'
 import Element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/zh-CN'
-import ElDataTable from '@femessage/el-data-table'
-import ElFormRenderer from '@femessage/el-form-renderer'
 
-Vue.component('el-form-renderer', ElFormRenderer)
-Vue.component('el-data-table', ElDataTable)
 Vue.use(Element, { locale })
